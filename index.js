ab2f9fb4494084dd692d994aca2727354514d974
rob <robertmaticke12@gmail.com>
2022-08-16 20:53:16 -0400

    commit tree 9559306e5786ccaa761a8645a0b963a637336434

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    let appendedCats = [...cats, name]
+    return appendedCats
+}
+
+function prependCat(name){
+    let prependedCats = [name, ...cats]
+    return prependedCats
+}
+
+function removeLastCat(){
+    let revisedCats = [...cats.slice(0, 2)]
+    return revisedCats
+}
+
+function removeFirstCat(){
+    let revisedCats2 = [...cats.slice(1)]
+    return revisedCats2
+}
